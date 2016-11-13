4de1b0e20436ab790877840d229300ede1c53fb2
Sean <shonum@gmail.com>
2016-11-12 17:08:21 -0800

    Complete factorial

--- src/recursion.js
+++ src/recursion.js
@@ -5,7 +5,13 @@
 // Example:  5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5);  // 120
 var factorial = function(n) {
+  if (n === 1) {
+    return 1;
+  }
+  return n * factorial(n-1);
 };
+
+
 
 // 2. Compute the sum of an array of integers.
 // Example:  sum([1, 2, 3, 4, 5, 6]);  // 21
